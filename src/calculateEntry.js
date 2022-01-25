82a1bf9fdf6343f91f3ccc10b48ddf2ccab7b9eb
Alline <alline_franciely@hotmail.com>
2022-01-25 19:25:03 -0300

    requisito 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,32 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const entrantsTotal = { child: 0, adult: 0, senior: 0 };
+  entrants.forEach((entrant) => {
+    if (entrant.age < 18) {
+      entrantsTotal.child += 1;
+    }
+    if (entrant.age >= 18 && entrant.age < 50) {
+      entrantsTotal.adult += 1;
+    }
+    if (entrant.age >= 50) {
+      entrantsTotal.senior += 1;
+    }
+  });
+  return entrantsTotal;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (typeof entrants === 'undefined' || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const { prices } = data; // sem as chaves é necessário usar o data.prices.
+  console.log(prices);
+  const entrantsObj = countEntrants(entrants);
+  const childEntry = entrantsObj.child * prices.child;
+  const adultEntry = entrantsObj.adult * prices.adult;
+  const seniorEntry = entrantsObj.senior * prices.senior;
+  return childEntry + adultEntry + seniorEntry;
 }
 
 module.exports = { calculateEntry, countEntrants };
--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,25 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if(!animal) {
+    let obj = {};
+    data.species.forEach((specie) => {
+      obj[specie.name] = specie.residents.length;
+  });
+  return obj;
+}
+if (!animal.sex) {
+  const bixos = data.species.find((bixo) =>  bixo.name === animal.specie);
+    return bixos.residents.length;
+} 
+let bixosSex = 0;
+bixos.residents.forEach((resident) => {
+  if (resident.sex === animal.sex) {
+    bixosSex += 1;
+  }
+});
+return bixosSex;
 }
 
 module.exports = countAnimals;
