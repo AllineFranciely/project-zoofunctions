2246b9d1b85eb617eb371f3704d6169378119d09
Alline <alline_franciely@hotmail.com>
2022-01-25 17:57:23 -0300

    requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,15 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return data.employees.some((employee) => employee.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId) === true) {
+    return data.employees.filter((employee) => employee.managers
+      .some((id) => id === managerId))
+      .map((name) => `${name.firstName} ${name.lastName}`);
+  } throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
 module.exports = { isManager, getRelatedEmployees };
