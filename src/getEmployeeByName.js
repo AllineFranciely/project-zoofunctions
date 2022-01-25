92efa35ec4f2a3eb5c266b40263d83d7fa3e1425
Alline <alline_franciely@hotmail.com>
2022-01-25 15:35:47 -0300

    requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (!employeeName) {
+    return {};
+  }
+  const employeeZoo = data.employees.find((employee) => (
+    employee.firstName === employeeName || employee.lastName === employeeName
+  ));
+  return employeeZoo;
 }
 
 module.exports = getEmployeeByName;
