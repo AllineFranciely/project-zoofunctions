66cac73d42efc03eba75e19defda90832b75da2d
Alline <alline_franciely@hotmail.com>
2022-01-25 15:17:01 -0300

    requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const arrayAnimais = data.species.find((species) => species.name === animal);
+  const idadeAnimais = arrayAnimais.residents.every((ages) => ages.age > age);
+  return idadeAnimais;
 }
 
 module.exports = getAnimalsOlderThan;
