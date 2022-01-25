93ec77282b86283b245ae75aea441bad4ee980ae
Alline <alline_franciely@hotmail.com>
2022-01-25 19:37:59 -0300

    requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,25 +1,24 @@
-const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  if(!animal) {
-    let obj = {};
+  if (!animal) {
+    const obj = {};
     data.species.forEach((specie) => {
       obj[specie.name] = specie.residents.length;
-  });
-  return obj;
-}
-if (!animal.sex) {
-  const bixos = data.species.find((bixo) =>  bixo.name === animal.specie);
-    return bixos.residents.length;
-} 
-let bixosSex = 0;
-bixos.residents.forEach((resident) => {
-  if (resident.sex === animal.sex) {
-    bixosSex += 1;
+    });
+    return obj;
   }
-});
-return bixosSex;
+  const bixos = data.species.find((bixo) => bixo.name === animal.specie);
+  if (!animal.sex) {
+    return bixos.residents.length;
+  }
+  let bixosSex = 0;
+  bixos.residents.forEach((resident) => {
+    if (resident.sex === animal.sex) {
+      bixosSex += 1;
+    }
+  });
+  return bixosSex;
 }
 
 module.exports = countAnimals;
