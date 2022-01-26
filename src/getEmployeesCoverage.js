e7ad12e9e11910736935b3227e1903125d0bbd72
Alline <alline_franciely@hotmail.com>
2022-01-26 15:45:17 -0300

    comentarios

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,14 +1,14 @@
 const data = require('../data/zoo_data');
 
 function getEmployee(person) {
-  return data.employees.find((employee) =>
+  return data.employees.find((employee) => // find encontra o primeiro elemento que satisfaz a condição.
     (person.name === employee.firstName
   || person.name === employee.lastName
   || person.id === employee.id));
 }
 
 function getSpecies(employee) {
-  return employee.responsibleFor.map((id) =>
+  return employee.responsibleFor.map((id) => // map junta no array todas as especies com o id correspondente.
     data.species.find((specie) =>
       (specie.id === id)));
 }
@@ -27,13 +27,13 @@ function getAllEmployees() {
 function getEmployeesCoverage(person) {
   if (!person) return getAllEmployees();
   const employee = getEmployee(person);
-  if (employee === undefined) throw new Error('Informações inválidas');
+  if (employee === undefined) throw new Error('Informações inválidas'); // cria a mensagem de erro diante da condição passada.
   const species = getSpecies(employee);
   return {
     id: employee.id,
     fullName: `${employee.firstName} ${employee.lastName}`,
-    species: species.map((specie) => specie.name),
-    locations: species.map((specie) => specie.location),
+    species: species.map((specie) => specie.name), // map junta no array todas as espécies da condição.
+    locations: species.map((specie) => specie.location), // map junta no array todas as localizações.
   };
 }
 
